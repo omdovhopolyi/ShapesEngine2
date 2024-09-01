f8e93b22e4b49c94080829dd97655a2ae897c698
OleksiiDovhopolyi <andovgopolyy@gmail.com>
2024-09-01 20:37:04 +0300

    fix system registration macro

--- src/ECS/SystemsFactoryRegistration.h
+++ src/ECS/SystemsFactoryRegistration.h
@@ -7,13 +7,14 @@
 #define SYSTEMS_FACTORY(Type) \
     public: \
         static bool registered; \
-        static bool RegisterFactory() \
-        { \
-            SystemsFactory::Instance().RegisterSystemsFactory(#Type, [](){ return std::make_unique<Type>(); }); \
-            return true; \
-        } \
+        static bool RegisterFactory(); \
         std::type_index GetTypeIndex() override { return std::type_index(typeid(Type)); } \
         std::string GetTypeName() override { return #Type; }
 
 #define REGISTER_SYSTEMS_FACTORY(Type) \
-    bool Type::registered = Type::RegisterFactory();
+    bool shen::Type::RegisterFactory() \
+    { \
+        shen::SystemsFactory::Instance().RegisterSystemsFactory(#Type, [](){ return std::make_unique<Type>(); }); \
+        return true; \
+    } \
+    bool shen::Type::registered = shen::Type::RegisterFactory();
