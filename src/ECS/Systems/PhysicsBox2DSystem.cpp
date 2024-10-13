2777a3b70a8b3abf60f832019687508d1825526f
DESKTOP-1B2D14T\Grabovsky <andovgopolyy@gmail.com>
2024-10-14 00:43:26 +0300

    refactor of physics system

--- src/ECS/Systems/PhysicsBox2DSystem.cpp
+++ src/ECS/Systems/PhysicsBox2DSystem.cpp
@@ -16,60 +16,14 @@ namespace shen
 
     void PhysicsBox2DSystem::Start()
     {
-        _world = std::make_unique<b2World>(b2Vec2(0.f, 0.f/*-9.8f*/));
-        if (!_world)
-        {
-            Assert(!_world, "[PhysicsBox2DSystem::Start] Can not create b2World");
-            return;
-        }
-
+        InitWorld();
         SetupDefaultCollisionListener();
-
-        auto& gameWorld = _systems->GetWorld();
-
-        gameWorld.Each<RigidBody, Transform>(
-            [&](const auto& entity, RigidBody& rb, Transform& transform)
-        {
-            b2BodyDef bodyDef;
-            bodyDef.type = static_cast<b2BodyType>(rb.type);
-            bodyDef.position.Set(transform.position.x / PxlPerMeter, transform.position.y / PxlPerMeter);
-            bodyDef.angle = Radians(transform.rotation);
-
-            auto dynamicBody = _world->CreateBody(&bodyDef);
-            dynamicBody->GetUserData().pointer = reinterpret_cast<uintptr_t>(&entity);
-
-            rb.body = dynamicBody;
-
-            b2PolygonShape dynamicBox;
-            dynamicBox.SetAsBox(rb.size.x / 2.f, rb.size.y / 2.f);
-
-            b2FixtureDef fixtureDef;
-            fixtureDef.shape = &dynamicBox;
-            fixtureDef.density = 1.f;
-            fixtureDef.isSensor = rb.sensor;
-
-            dynamicBody->CreateFixture(&fixtureDef);
-        });
+        InitRigidBodies();
     }
 
     void PhysicsBox2DSystem::Update()
     {
-        auto& gameWorld = _systems->GetWorld();
-        auto time = _systems->GetSystem<TimeSystem>();
-
-        int32 velocityIterations = 6;
-        int32 positionIterations = 2;
-        _world->Step(time->Dt(), velocityIterations, positionIterations);
-
-        gameWorld.Each<RigidBody, Transform>(
-            [&](const auto entity, RigidBody& rb, Transform& transform)
-        {
-            auto position = rb.body->GetPosition();
-            const auto angle = rb.body->GetAngle();
-
-            transform.position = sf::Vector2f(position.x * PxlPerMeter, position.y * PxlPerMeter);
-            transform.rotation = Degrees(angle);
-        });
+        UpdatePhysics();
     }
 
     void PhysicsBox2DSystem::Stop()
@@ -88,5 +42,67 @@ namespace shen
     {
         _collisionListener = std::move(listener);
         _world->SetContactListener(_collisionListener.get());
+    }
+
+    bool PhysicsBox2DSystem::InitWorld()
+    {
+        _world = std::make_unique<b2World>(b2Vec2(0.f, 0.f/*-9.8f*/));
+        if (!_world)
+        {
+            Assert(!_world, "[PhysicsBox2DSystem::Start] Can not create b2World");
+            return false;
+        }
+
+        return true;
+    }
+
+    void PhysicsBox2DSystem::InitRigidBodies()
+    {
+        auto& gameWorld = _systems->GetWorld();
+
+        gameWorld.Each<RigidBody, Transform>(
+            [&](const auto& entity, RigidBody& rb, Transform& transform)
+        {
+            if (!rb.body)
+            {
+                b2BodyDef bodyDef;
+                bodyDef.type = static_cast<b2BodyType>(rb.type);
+                bodyDef.position.Set(transform.position.x / PxlPerMeter, transform.position.y / PxlPerMeter);
+                bodyDef.angle = Radians(transform.rotation);
+
+                auto dynamicBody = _world->CreateBody(&bodyDef);
+                dynamicBody->GetUserData().pointer = reinterpret_cast<uintptr_t>(&entity);
+
+                rb.body = dynamicBody;
+
+                b2PolygonShape dynamicBox;
+                dynamicBox.SetAsBox(rb.size.x / 2.f, rb.size.y / 2.f);
+
+                b2FixtureDef fixtureDef;
+                fixtureDef.shape = &dynamicBox;
+                fixtureDef.density = 1.f;
+                fixtureDef.isSensor = rb.sensor;
+
+                dynamicBody->CreateFixture(&fixtureDef);
+            }
+        });
+    }
+
+    void PhysicsBox2DSystem::UpdatePhysics()
+    {
+        auto& gameWorld = _systems->GetWorld();
+        auto time = _systems->GetSystem<TimeSystem>();
+
+        _world->Step(time->Dt(), _velocityIterations, _positionIterations);
+
+        gameWorld.Each<RigidBody, Transform>(
+            [&](const auto entity, RigidBody& rb, Transform& transform)
+        {
+            auto position = rb.body->GetPosition();
+            const auto angle = rb.body->GetAngle();
+
+            transform.position = sf::Vector2f(position.x * PxlPerMeter, position.y * PxlPerMeter);
+            transform.rotation = Degrees(angle);
+        });
     }
 }
--- src/ECS/Systems/PhysicsBox2DSystem.h
+++ src/ECS/Systems/PhysicsBox2DSystem.h
@@ -20,8 +20,17 @@ namespace shen
         void SetupDefaultCollisionListener();
         void SetCollisionListener(std::unique_ptr<CollisionListener>& listener);
 
+        bool InitWorld();
+        void InitRigidBodies();
+
+    private:
+        void UpdatePhysics();
+
     private:
         std::unique_ptr<b2World> _world;
         std::unique_ptr<CollisionListener> _collisionListener;
+
+        int32 _velocityIterations = 6;
+        int32 _positionIterations = 2;
     };
 }
