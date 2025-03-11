453e3776480b790563b9bd6aa32c804ebfb794de
DESKTOP-1B2D14T\Grabovsky <andovgopolyy@gmail.com>
2025-03-11 23:09:52 +0200

    hiding console (winmain)

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 
 #include "Game/Game.h"
 
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
 int main(int argc, char* argv[])
+#endif
 {
     shen::Game game;
 
